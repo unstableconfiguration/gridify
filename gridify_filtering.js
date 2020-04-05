b4e8c42ede4c979e2ce70178acfae09b617b6006
oldmanmuffins <mluper@protonmail.com>
2020-04-05 13:39:10 -0400

    syntax cleanup on filtering

--- gridify_filtering.js
+++ gridify_filtering.js
@@ -2,73 +2,82 @@
 Gridify.prototype.extensions.filtering = function(div){
     let grid = this;
 
-    // add header row 
-    let on_header_initialized = grid.header.onInitialized;
-    grid.header.onInitialized = function(header_options){
+    let onHeaderInitialized = grid.header.onInitialized;
+    grid.header.onInitialized = function(header_options) {
+        onHeaderInitialized(); 
+
         let filter_row = grid.table.tHead.insertRow();
         filter_row.id = grid.table.id + '_filters'
     }
 
     let onColumnAdded = grid.header.onColumnAdded;
-    grid.header.onColumnAdded = function(header_cell, column_definition){
-        onColumnAdded(header_cell, column_definition);
-        let filter_cell = grid.table.tHead.rows[1].insertCell();
-        if(!column_definition.filter) return;
-        filter_cell.id = grid.table.id + '_filters_' + column_definition.field;
-        grid.filtering.add_filter(filter_cell, column_definition.field, column_definition.filter);
+    grid.header.onColumnAdded = function(headerCell, columnDefinition){
+        onColumnAdded(headerCell, columnDefinition);
+
+        grid.filtering.addFilter(columnDefinition.field, columnDefinition.filter);
     }
 
-    let filter_pending = 0;
-    let filter_delay = function(callback) {
+    let filterPending = 0;
+    let filterDelay = function(callback) {
         return function(field_value, filter_value) {
-            clearTimeout(filter_pending);
-            filter_pending = setTimeout(()=>{ callback(field_value, filter_value); }, 150);
+            clearTimeout(filterPending);
+            filterPending = setTimeout(()=>{ callback(field_value, filter_value); }, 150);
         }
     }
+
     grid.filtering = { 
-        filter : function(){
-            let filter_controls = grid.filtering.filter_controls();
-            let grid_data = grid.data.get();
-            
-            grid.body.rows.forEach((row, i)=>{
-                let filtered_out = filter_controls.some((filter_control)=>{
-                    let cell_value = grid_data[i][filter_control.property];
-                    return !filter_control.rule(cell_value, filter_control.value);
-                });
-                row.filtered = filtered_out;
-                row.style.display = filtered_out ? 'none' : ''
-            }); 
-        }
-        , filter_callback : function() {
-            return filter_delay(grid.filtering.filter());
-        }
-        , cells : function() { return Array.from(grid.table.tHead.rows[1].cells); }
-        , filter_controls : function(){
-            return grid.filtering.cells().map(cell => cell.firstChild).filter(x => !!x);
-        }
-        , add_filter : function(filter_cell, field_name, options) {
-            options = grid.filtering._set_default_options(field_name, options);
+        addFilter : function(field, options) {
+            let cell = grid.filtering._addFilterCell(field)
+            if(!options) { return; }
+            options = grid.filtering._getFilterOptions(field, options);
+
             let control = options.control;
             control.rule = options.rule;
-            control.property = field_name;
-            control.addEventListener(options.event, grid.filtering.filter_callback);
-            filter_cell.appendChild(control);
+            control.property = field;
+            control.addEventListener(options.event, filterDelay(grid.filtering.filter));
+            cell.appendChild(control);
         }
-        , _set_default_options : function(field_name, options){
-            if(typeof(options) !== 'object') options = {};
-            options.rule = options.rule || grid.filtering._default_filter_rule;
-            options.control = options.control || grid.filtering._default_filter_control(field_name);
+        , cells : function() { return Array.from(grid.table.tHead.rows[1].cells); }
+        , filter : function(){
+            let filterControls = grid.filtering.getControls();
+            let gridData = grid.data.get();
+            
+            grid.body.rows.forEach((row, i)=>{
+                let filteredOut = filterControls.some((filterControl)=>{
+                    let cellValue = gridData[i][filterControl.property];
+                    return !filterControl.rule(cellValue, filterControl.value);
+                });
+                row.filtered = filteredOut;
+                row.style.display = filteredOut ? 'none' : ''
+            }); 
+        }
+        , getControls : function(){
+            return grid.filtering.cells().map(cell => cell.firstChild).filter(x => !!x);
+        }
+        , _addFilterCell : function(field) {
+            let cell = grid.table.tHead.rows[1].insertCell();
+            cell.id = grid.table.id + '_filters_' + field;
+            return cell;
+        }
+        , defaultFilterRule : function(cellValue, fieldValue) {
+            return (''+cellValue).toLowerCase()
+                .substr(0, fieldValue.length) === fieldValue.toLowerCase();
+        }
+        , _getFilterOptions : function(field, options) { 
+            if(typeof(options) === 'function') { options = { rule : options } };
+            if(typeof(options) !== 'object') { options = { } };
+
+            options.rule = options.rule || grid.filtering.defaultFilterRule;
+            options.control = options.control || grid.filtering._getDefaultFilterControl(field);
             options.event = options.event || 'keyup';
+
             return options;
         }
-        , _default_filter_rule : function(x, y){ 
-            return (''+x).toLowerCase().substr(0, y.length) == y.toLowerCase();
-        }
-        , _default_filter_control : function(property_name){
+        , _getDefaultFilterControl : function(field){
             let control = document.createElement('input');
             control.type = 'text';
-            control.id = grid.table.id + '_filter_' + property_name;
-            control.style = 'width: 80%; display: block; margin: auto;'
+            control.id = grid.table.id + '_fiter_' + field;
+            control.style = 'width:80%; display: block; margin: auto;';
             return control;
         }
     }
