3853f4527c7f442642807faa15f4cfd8d61869be
oldmanmuffins <mluper.official@protonmail.com>
2021-04-08 12:47:33 -0400

    updating filter tests

--- tests/filtering-tests.js
+++ tests/filtering-tests.js
@@ -3,22 +3,20 @@ import { Gridify } from '../src/gridify.js';
 export const FilteringTests = function() {
     describe('Filtering', function(){
         let assert = chai.assert;
-        
-        let newgrid = function(id){
-            let div = document.createElement('div');
-            div.id = id;
-            div.style.display = 'none';
-            document.body.appendChild(div);
-            return new Gridify(id);
-        }
 
+        let getHiddenRows = function(grid) { 
+            return Array.from(grid.html.tBodies[0].rows)
+                .filter(row => row.style.display == 'none');
+        }
+        
         it('Adds a filter textbox on filterable columns', function() {
             let grid = new Gridify({
                 columns : [ { field : 'Col', header : 'Col', filter : true } ],
                 data : [ { Col : 'a' } ]
             });
-            assert.isTrue(grid.html.tHead.rows[1].cells[0].firstChild != undefined);
+            assert(grid.html.tHead.rows[1].cells[0].firstChild.id == 'new-grid-filters-Col');
         });
+
         it('Defaults to xyz% filtering function', function(){
             let grid = new Gridify({
                 columns : [ { field : 'Col', header : 'Col', filter : true } ],
@@ -27,17 +25,12 @@ export const FilteringTests = function() {
 
             let filterTextbox = grid.html.tHead.rows[1].cells[0].firstChild;
             filterTextbox.value = 'a';
-            grid.filtering.filter();
-            assert.isTrue(
-                Array.from(grid.html.tBodies[0].rows)
-                    .filter(r => r.style.display == 'none').length == 1
-            );
+            grid.filter();
+            assert(getHiddenRows(grid).length == 1);
+            
             filterTextbox.value = 'aa';
-            grid.filtering.filter();
-            assert.isTrue(
-                Array.from(grid.html.tBodies[0].rows)
-                    .filter(r => r.style.display == 'none').length == 2
-            );
+            grid.filter();
+            assert(getHiddenRows(grid).length == 2);
         });
         it('Applies all filters to the data set', function() {
             let grid = new Gridify({
@@ -54,11 +47,8 @@ export const FilteringTests = function() {
 
             grid.html.tHead.rows[1].cells[0].firstChild.value = 'a';
             grid.html.tHead.rows[1].cells[1].firstChild.value = 'b';
-            grid.filtering.filter();
-            assert.isTrue(
-                Array.from(grid.html.tBodies[0].rows)
-                    .filter(r => r.style.display == 'none').length == 3
-            );
+            grid.filter();
+            assert(getHiddenRows(grid).length == 3);
         });
         it('Allows for custom filter logic', function() {
             let grid = new Gridify({
@@ -67,7 +57,7 @@ export const FilteringTests = function() {
                         field : 'Col', 
                         header : 'Col A',
                         filter : { 
-                            rule : (cellValue, filterValue) => {
+                            compare : (cellValue, filterValue) => {
                                 return cellValue.includes(filterValue);
                             }
                         }
@@ -77,47 +67,34 @@ export const FilteringTests = function() {
             });
 
             grid.html.tHead.rows[1].cells[0].firstChild.value = 'bc';
-            grid.filtering.filter();
-            assert.isTrue(
-                Array.from(grid.html.tBodies[0].rows)
-                    .filter(r => r.style.display == 'none').length == 1
-            );
+            grid.filter();
+            assert(getHiddenRows(grid).length == 1);
         });
         it('Allows for custom filter control', function() {
-            let ddl = document.createElement('select');
-            let s0 = document.createElement('option');
-            s0.value = 0; s0.innerHTML = 'zero';
-            ddl.appendChild(s0);
-            let s1 = document.createElement('option');
-            s1.value = 1; s1.innerHTML = 'one'
-            ddl.appendChild(s1);
-            let s2 = document.createElement('option');
-            s2.value = 2; s2.innerHTML = 'two'
-            ddl.appendChild(s2);
-            
+            let chk = document.createElement('input');
+            chk.type = 'checkbox'
+  
             let grid = new Gridify({ 
                 columns : [ 
                     { 
                         field : 'Col', 
                         header : 'Col A',
                         filter : {
-                                control : ddl,
-                                rule : function(cellValue, filterValue) {
+                                control : chk,
+                                compare : function(cellValue, filterValue) {
                                     if(+filterValue === 0) { return true; }
                                     return +filterValue === +cellValue;
-                                },
-                                event : 'change'
+                                }
                             }
                         }  
                     ],
-                data : [ { Col : 1 }, { Col : 2 }, { Col : 3 } ]
+                data : [ { Col : 1 }, { Col : 0 }, { Col : 1 } ]
             });
-
+            //chk.addEventListener('change', () => { grid.filter() });
+            
             grid.html.tHead.rows[1].cells[0].firstChild.value = 1;
-            grid.filtering.filter();
-            assert.isTrue(
-                Array.from(grid.html.tBodies[0].rows)
-                    .filter(r => r.style.display == 'none').length == 2);
+            grid.filter();
+            assert(getHiddenRows(grid).length == 1);
         });
     });
 }
