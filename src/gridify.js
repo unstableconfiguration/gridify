2d0bc6cf8fb905c81e6664967541995932298ce0
oldmanmuffins <mluper.official@protonmail.com>
2021-04-09 11:01:57 -0400

    no longer embedding options in els

--- src/gridify.js
+++ src/gridify.js
@@ -27,14 +27,13 @@ export const Gridify = function(options = {}) {
         }
     }
 
-
     grid.onTableCreated = function(table, options) { if(options.onTableCreated) { options.onTableCreated(table, options); } }
     grid.onCaptionCreated = function(caption, captionDefinition) { if(options.onCaptionCreated) { options.onCaptionCreated(caption, captionDefinition); } }
     grid.onHeaderCreated = function(tHead, headers) { if(options.onHeaderCreated) { options.onHeaderCreated(tHead, headers); } }
-    grid.onHeaderCellCreated = function(th, headerDefinition) { if(options.onHeaderCellCreated) { options.onHeaderCellCreated(th, headerDefinition); } }
+    grid.onHeaderCellCreated = function(th, column) { if(options.onHeaderCellCreated) { options.onHeaderCellCreated(th, column); } }
     grid.onTableBodyCreated = function(tBody, columns) { if(options.onTableBodyCreated) { options.onTableBodyCreated(tBody, columns); } }
     grid.onTableRowCreated = function(tr, columns) { if(options.onTableRowCreated) { options.onTableRowCreated(tr, columns); } }
-    grid.onTableCellCreated = function(td, columnDefinition) { if(options.onTableCellCreated) { options.onTableCellCreated(td, columnDefinition); } }
+    grid.onTableCellCreated = function(td, column) { if(options.onTableCellCreated) { options.onTableCellCreated(td, column); } }
     grid.onFooterCreated = function(tFoot, footers) { if(options.onFooterCreated) { options.onFooterCreated(tFoot, footers); } }
     grid.onFooterCellCreated = function(td, footerDefinition) { if(options.onFooterCellCreated) { options.onFooterCellCreated(td, footerDefinition); } }
 
@@ -59,7 +58,7 @@ export const Gridify = function(options = {}) {
         , initialize : function(options) {
             _table = document.createElement('table');
             _table.id = grid.table._getTableId(options);
-            _table.options = options;
+            _table.options = grid.table.__options(options);
             return _table;
         }
         , _getTableId : function(options) {
@@ -68,124 +67,117 @@ export const Gridify = function(options = {}) {
             if(grid.container) { return grid.container.id + '-grid'; }
             return 'new-grid';
         }
+        , __options : function(options) { 
+            if(!options.columns) {
+                options.columns = [];
+                if(Array.isArray(options.data) && options.data.length > 0){
+                    for(let field in options.data[0]) {
+                        options.columns.push({
+                            field : field
+                        });
+                    }
+                }
+            }
+
+            return options;
+        }
     }
     Object.defineProperty(grid, 'html', { get : () => _table });
 
     grid.caption = {
-        create : function(captionOptions) {
-            if(!captionOptions) { return; }
-            let caption = grid.caption.initialize(captionOptions);
+        create : function(caption) {
+            if(!caption) { return; }
+            let caption = grid.caption.initialize(caption);
 
-            _setAttributes(caption, caption.options.attributes);
-            caption.innerText = caption.options.text;
+            let options = grid.caption.__options(caption);
+            _setAttributes(caption, options.attributes);
+            caption.innerText = options.text;
 
-            grid.onCaptionCreated(caption, caption.options);
+            grid.onCaptionCreated(caption, options);
         }
-        , initialize : function(captionOptions) {
+        , initialize : function(caption) {
             let caption = _table.createCaption();
-
             caption.id = _table.id + '-caption';
-            caption.options = typeof(captionOptions) === 'string' 
-                ? { text : captionOptions } 
-                : captionOptions;
 
             return caption;
+        }
+        , __options : function(caption) {
+            return typeof(caption) === 'string'
+                ? { text : caption }
+                : caption;
         }
     }
 
     grid.header = {
         create : function(columns) {
             if(!columns) { return ; }
-            let tHead = grid.header.initialize(columns);
+            let tHead = grid.header.initialize();
 
-            grid.header.addHeaderCells();
+            grid.header.addHeaderCells(columns);
 
-            grid.onHeaderCreated(tHead, tHead.options);
+            grid.onHeaderCreated(tHead, columns);
         }
-        , initialize : function(columns) {
+        , initialize : function() {
             if(_table.tHead) { _table.removeChild(_table.tHead); }
             let tHead = _table.createTHead();
-
             tHead.id = _table.id + '-thead';
-            tHead.options = grid.header._parseOptions(columns);
             
             return tHead;
         }
-        , _parseOptions : function(columns) {
-            return columns.map(opt => {
-                if(typeof(opt.header) === 'string') { 
-                    opt.header = { text : opt.header } 
-                }
-                return opt;
-            });
-        }
-        , addHeaderCells : function() {
+        , addHeaderCells : function(columns) {
             let hr = _table.tHead.insertRow();
-            _table.tHead.options
-                .forEach(o => { grid.header.addHeaderCell(hr, o); });
+            columns.forEach(col => { grid.header.addHeaderCell(hr, col); });
         }
-        , addHeaderCell : function(headerRow, columnDefinition) {
+        , addHeaderCell : function(headerRow, column) {
             let th = document.createElement('th');
-            th.id = _table.tHead.id + '-' + columnDefinition.field || headerRow.cells.length;
+            th.id = _table.tHead.id + '-' + column.field || headerRow.cells.length;
             headerRow.appendChild(th);
 
-            if(columnDefinition.header) {
-                th.innerText = columnDefinition.header.text || '';
-                _setAttributes(th, columnDefinition.header.attributes);
-            }
+            let options = grid.header.__options(column);
+            th.innerText = options.text;
+            _setAttributes(th, options.attributes);
 
-            grid.onHeaderCellCreated(th, columnDefinition); 
+            grid.onHeaderCellCreated(th, column); 
+        }
+        , __options : function(column) { 
+            if(!column.header) { return {}; }
+            if(typeof(column.header) === 'string') { return { text : column.header } }
+            return column.header;
         }
     }
 
     grid.body = {
-        create : function(data, columns = options.columns) {
-            let tBody = grid.body.initialize(columns);
+        initialize : function() {  
+            while(_table.tBodies.length) { _table.removeChild(_table.tBodies[0]); }
+            let tBody = _table.createTBody();
+            tBody.id = _table.id + '-tbody';
+            return tBody;
+        }
+        , clear : function() { _clear(_table.tBodies[0]); }
+        , create : function(data, columns) {
+            let tBody = grid.body.initialize();
+
+            data.forEach(row => {
+
+            });
 
             for(let idx in data) {
                 grid.body.addTableRow(tBody, idx, data[idx]);
             }
 
-            grid.onTableBodyCreated(tBody, tBody.options);
+            grid.onTableBodyCreated(tBody, columns);
         }
-        , initialize : function(columns) {  
-            while(_table.tBodies.length) { _table.removeChild(_table.tBodies[0]); }
-            let tBody = _table.createTBody();
-            tBody.id = _table.id + '-tbody';
-            tBody.options = columns;
-            return tBody;
-        }
-        , _parseColumns : function(columns) {
-            return columns.map(col => {
-                if(typeof(col) === 'string') { col = { text : col }; }
-            });
-        }
-        , clear : function() { _clear(_table.tBodies[0]); }
-        , getColumnDefinition : function(field) {
-            let colDefs = _table.tBodies[0].options;
-            if(!colDefs) { return; }
-            return colDefs.find(d => d.field == field);
-        }
-        , addTableRow : function(tBody, ridx, rowData) {
+        , addTableRow : function(tBody, rowData) {
             let tr = tBody.insertRow();
-            tr.id = tBody.id + '-' + ridx;
+            tr.id = tBody.id + '-' + tBody.rows.length;
 
-            let colDefs = tBody.options;
-            if(colDefs){
-                for(let d in colDefs) {
-                    let field = colDefs[d].field;
-                    grid.body.addTableCell(tr, field, rowData[field])
-                }
-            }
-            else { 
-                for(let field in rowData) {
-                    grid.body.addTableCell(tr, field, rowData[field]);
-                }
-            }
+            _table.options.columns.forEach(col => {
+                grid.body.addTableCell(tr, col, rowData[col.field]);
+            });
 
             grid.onTableRowCreated(tr);
         }
-        , addTableCell : function(tr, field, value) {
+        , addTableCell : function(tr, column, value) {
             let td = tr.insertCell();
             td.id = tr.id + '-' + field;
 
@@ -193,11 +185,10 @@ export const Gridify = function(options = {}) {
             td.value = value;
             td.innerText = value;
 
-            let colDef = grid.body.getColumnDefinition(field);
-            if(colDef && colDef.attributes) { _setAttributes(td, colDef.attributes); }
-            if(colDef && colDef.click) { td.onclick = colDef.click; }
+            _setAttributes(td, column.attributes);
+            if(column.click) { td.onclick = column.click; }
 
-            grid.onTableCellCreated(td, colDef);
+            grid.onTableCellCreated(td, column);
         }
     }
 
@@ -220,45 +211,45 @@ export const Gridify = function(options = {}) {
 
     grid.footer = { 
         create : function(columns) {
-            if(!columns) { return ; }
+            if(!columns.find(col => col.footer)) { return ; }
             let tFoot = grid.footer.initialize(columns);
 
-            grid.footer.addFooterCells();
+            grid.footer.addFooterCells(tFoot, columns);
 
-            grid.onFooterCreated(tFoot, tFoot.options);
+            grid.onFooterCreated(tFoot, columns);
         }
-        , initialize : function(columns) {
+        , initialize : function() {
             if(_table.tFoot) { _table.removeChild(_table.tFoot); }
             let tFoot = _table.createTFoot();
             
             tFoot.id = _table.id + '-tfoot';
-            tFoot.options = grid.footer._parseOptions(columns);;
             
             return tFoot;
         }
-        , _parseOptions : function(columns) {
-            return columns.map(opt => { 
-                if(typeof(opt.footer) === 'string') {
-                    opt.footer = { text : opt.footer };
-                }
-                return opt;
+        , addFooterCells : function(tFoot, columns) {
+            let tr = tFoot.insertRow();
+            columns.forEach(column => { 
+                grid.footer.addFooterCell(tr, column); 
             });
         }
-        , addFooterCells : function() {
-            let fr = _table.tFoot.insertRow();
-            _table.tFoot.options
-                .forEach(o => { grid.footer.addFooterCell(fr, o); });
-        }
-        , addFooterCell : function(footerRow, columnDefinition) {
-            let td = footerRow.insertCell();
-            td.id = _table.tFoot.id + '-' + columnDefinition.field || footerRow.cells.length;
+        , addFooterCell : function(tr, column) {
+            let td = tr.insertCell();
+            td.id = _table.tFoot.id + '-' + column.field || tr.cells.length;
 
-            if(columnDefinition.footer) {
-                td.innerText = columnDefinition.footer.text || '';
-                _setAttributes(td, columnDefinition.footer.attributes);
+            let footer = grid.footer.__options(column);
+            if(footer) {
+                td.innerText = footer.text;
+                _setAttributes(td, footer.attributes);
             }
 
-            grid.onFooterCellCreated(td, columnDefinition);
+            grid.onFooterCellCreated(td, column);
+        }
+        , __options : function(column) { 
+            if(!column.footer) { return; }
+            if(typeof(column.footer) === 'string') {
+                return { text : column.footer };
+            }
+            return column.footer;
         }
     }
 
