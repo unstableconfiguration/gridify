f4714fd1ef8a3c69331771a2008a297db7d51585
oldmanmuffins <mluper.official@protonmail.com>
2021-09-02 21:11:46 -0400

    changing up pager

--- app/modules/gridify-paging.js
+++ app/modules/gridify-paging.js
@@ -1,5 +1,6 @@
 export const paging = function() {
     let grid = this;
+    grid.page = function(...args) { grid.paging.page(...args); };
 
     let onTableCreated = grid.onTableCreated;
     grid.onTableCreated = function(table, options) { 
@@ -12,71 +13,69 @@ export const paging = function() {
             let pagingRow = grid.html.tFoot.insertRow();
             pagingRow.id = grid.html.id + '-paging';
             pagingRow.options = options; 
-
-            let leftCell = pagingRow.insertCell();
-            leftCell.id = grid.html.id + '-paging-left';
-            leftCell.style = 'width:33%;';
-
-            let centerCell = pagingRow.insertCell();
-            centerCell.id = grid.html.id + '-paging-center';
-            centerCell.style = 'width:33%;';
-            centerCell.appendChild(grid.footer.pager.pagingControl(options));
-
-            let rightCell = pagingRow.insertCell();
-            rightCell.id = grid.html.id + '-paging-right';
-            rightCell.style = 'width:33%;'    
-        }
-        , setPage : function(pageNumber){
-            let textbox = document.getElementById(grid.html.id + '-paging-center-textbox');
-            if(textbox) { textbox.value = pageNumber; }
-            // set row counter when up
-        }
-        , pagingControl : function(options){
-            let container = document.createElement('div');
-            container.style = 'width:120px'
-
-            let textbox = document.createElement('input');
-            textbox.id = grid.html.id + '-paging-center-textbox';
-            textbox.className = 'pager-textbox';
-            textbox.value = options.currentPage || 1;
-            textbox.addEventListener('change', () => {
-                grid.paging.page(textbox.value < options.totalPages ? +textbox.value : options.totalPages);
-            });
-
-            let label = document.createElement('span');
-            label.style = 'width:40px;vertical-align:top';
-            label.innerText = ' of ' + options.totalPages || 1;
-
-            let leftArrow = document.createElement('div');
-            leftArrow.className = 'pager-left';
-            leftArrow.onclick = () => 
-                grid.paging.page(textbox.value > 1 ? +textbox.value -1 : 1);
+            // Prototype default pager.
+            //pagingRow.appendChild(grid.footer.pager.__defaultPagerControl());
+        },
+        /*__defaultPagerControl : function() { 
+            let pagerDiv = document.createElement('div');
+            pagerDiv.id = grid.html.id + '-pager';
             
-            let rightArrow = document.createElement('div');
-            rightArrow.className = 'pager-right';
-            rightArrow.onclick = () => 
-                grid.paging.page(textbox.value < options.totalPages ? +textbox.value +1  : options.totalPages); 
+            pagerDiv.appendChild(this.__elPrev());
+            pagerDiv.appendChild(this.__elDisplay());
+            pagerDiv.appendChild(this.__elNext());
 
-            container.appendChild(leftArrow);
-            container.appendChild(textbox);
-            container.appendChild(label);
-            container.appendChild(rightArrow);
-
-            return container;
-        }
-    }
+            return pagerDiv;
+        },
+        __elPrev : function() { 
+            let prev = document.createElement('span');
+            prev.innerHTML = "Prev ";
+            prev.addEventListener('click', e => {
+                let data = grid.paging.data;
+                if(data.currentPage == 1) { return; }
+                grid.paging.data.currentPage--;
+                grid.footer.pager.__updateDisplay();
+                grid.page(grid.paging.data.currentPage);
+            });
+            return prev;
+        },
+        __elNext : function() { 
+            let next = document.createElement('span');
+            next.innerHTML = " Next";
+            next.addEventListener('click', e => {
+                let data = grid.paging.data;
+                if(data.currentPage == data.visiblePages) { return; }
+                grid.paging.data.currentPage++;
+                grid.footer.pager.__updateDisplay();
+                grid.page(grid.paging.data.currentPage);
+            });
+            return next;
+        },
+        __elDisplay : function() {
+            let display = document.createElement('span');
+            let data = grid.paging.data;
+            display.id = grid.html.id + '-pager-display';
+            display.innerHTML = "Page 1 of " + data.visiblePages;
+            return display;
+        },
+        __updateDisplay : function() {
+            let display = document.querySelector("#" + grid.html.id + "-pager-display");
+            let data = grid.paging.data;
+            display.innerHTML = "Page " + data.currentPage + " of " + data.visiblePages;
+        }*/
+     }
 
     grid.paging = { 
         initialize : function(options){
             if(!options) { return; }
 
             grid.paging.extendSorting();
-            grid.paging.extendFiltering();
+            grid.paging.extendFiltering();          
 
-            options = grid.paging._defaultOptions(options);
-            grid.html.options.paging = options;
+            let pagerData = grid.paging._defaultOptions(options);
+            grid.paging.data = pagerData;
+
             grid.footer.pager.initialize(options); 
-            grid.paging.page(options.currentPage);
+            grid.paging.page(pagerData.currentPage);
         }
         // Not sure how to make these modules agnostic of one another. 
         // In the meantime, paging needs to know about sorting and filtering.
@@ -86,7 +85,7 @@ export const paging = function() {
                 grid.sorting.sort = function(options = {}){
                     grid.paging.clear();
                     sort(options);
-                    let currentPage = grid.html.options.paging.currentPage;    
+                    let currentPage = grid.paging.data.currentPage;    
                     grid.paging.page(currentPage);
                 }
             }
@@ -98,24 +97,30 @@ export const paging = function() {
                     grid.paging.clear();
                     filter(); 
                     grid.paging.page();
+                    //grid.footer.pager.__updateDisplay();
                 }
             }
         }
         , page : function(pageNumber = 1) {
-            grid.html.options.paging.currentPage = pageNumber;
-            grid.paging._setFooterValues(pageNumber);
+            grid.paging.data.currentPage = pageNumber;
+            grid.paging.data.visiblePages = grid.paging._getVisiblePageCount();
             grid.paging._setRowVisibility(pageNumber);
         }
         , clear : function() { 
             let rows = Array.from(grid.html.tBodies[0].rows);
             rows.forEach(r => { if(r.paged) { r.paged = undefined; r.style.display = ''; } });
         }
-        , _setRowVisibility : function(pageNumber){
+        // More blending of this with filtering. 
+        , _getVisiblePageCount : function() { 
             let rows = Array.from(grid.html.tBodies[0].rows);
-            let options = grid.html.options.paging;
+            rows = rows.filter(r => !r.filtered);
+            return Math.ceil(rows.length / grid.paging.data.rows);;
+        }
+        , _setRowVisibility : function(pageNumber) {
+            let rows = Array.from(grid.html.tBodies[0].rows);
+            let options = grid.paging.data;
             
             grid.paging.clear();
-            
             let start = (options.currentPage - 1) * options.rows;
             let end = options.currentPage * options.rows;
 
@@ -125,14 +130,12 @@ export const paging = function() {
 
             rows.forEach(r => {r.style.display = 'none'; r.paged = true; });
         }
-        , _setFooterValues : function(pageNumber){
-            grid.footer.pager.setPage(pageNumber);
-        }
         , _defaultOptions : function(options){
             if(typeof(options) !== 'object') options = {};
             options.rows = options.rows || 20;
             options.totalRows = options.totalRows || grid.data.get().length;
             options.totalPages = Math.ceil(options.totalRows/options.rows);
+            options.visiblePages = options.totalPages;
             options.currentPage = options.currentPage || 1;
             return options;
         }
